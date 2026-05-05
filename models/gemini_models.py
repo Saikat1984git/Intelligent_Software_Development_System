603ed58f45281df13d4aa63831cc95e1c4626aef
debmainak <mainakd@interrait.com>
2026-05-05 21:00:41 +0530

    openrouter integrated

--- models/gemini_models.py
+++ models/gemini_models.py
@@ -1,54 +1,55 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file if it exists
 
-API_KEY = os.environ.get("GEMINI_API_KEY")
+load_dotenv()
+
+API_KEY = os.environ.get("OPENROUTER_API_KEY")
 
 COMMON_CONFIG = dict(
     temperature=0.2,
-    api_key=API_KEY
+    api_key=API_KEY,
+    base_url="https://openrouter.ai/api/v1",
+    # default_headers={
+    #     "HTTP-Referer": os.environ.get("OPENROUTER_SITE_URL", "http://localhost"),
+    #     "X-Title": os.environ.get("OPENROUTER_SITE_NAME", "My App"),
+    # },
 )
 
 # ----------------------------
-# Gemini 3
+# "Gemini 3" Equivalent
 # ----------------------------
 
-
-GEMINI_31_PRO = ChatGoogleGenerativeAI(
-    model="gemini-3.1-pro-preview",
+GEMINI_31_PRO = ChatOpenAI(
+    model="google/gemini-3.1-pro-preview",   # High reasoning
     **COMMON_CONFIG
 )
 
-GEMINI_3_FLASH = ChatGoogleGenerativeAI(
-    model="gemini-3-flash-preview",
+GEMINI_3_FLASH = ChatOpenAI(
+    model="google/gemini-3-flash-preview",   # Fast + cheap
     **COMMON_CONFIG
 )
 
-GEMINI_3_FLASH_LITE = ChatGoogleGenerativeAI(
-    model="gemini-3.1-flash-lite-preview",
+GEMINI_3_FLASH_LITE = ChatOpenAI(
+    model="google/gemini-3.1-flash-lite-preview",  # Ultra cheap
     **COMMON_CONFIG
 )
-# GEMINI_3_1_FLASH_LITE = ChatGoogleGenerativeAI(
-#     model="gemini-3.1-flash-lite",
-#     **COMMON_CONFIG
-# )
 
 # ----------------------------
-# Gemini 2.5
+# "Gemini 2.5" Equivalent
 # ----------------------------
 
-GEMINI_25_PRO = ChatGoogleGenerativeAI(
-    model="gemini-2.5-pro",
+GEMINI_25_PRO = ChatOpenAI(
+    model="google/gemini-2.5-pro",   # Stable + strong reasoning
     **COMMON_CONFIG
 )
 
-GEMINI_25_FLASH = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+GEMINI_25_FLASH = ChatOpenAI(
+     model="google/gemini-2.5-flash",   # Stable + fast
     **COMMON_CONFIG
 )
 
-GEMINI_25_FLASH_LITE = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash-lite",
+GEMINI_25_FLASH_LITE = ChatOpenAI(
+    model="google/gemini-2.5-flash-lite-preview-09-2025",  # Cheapest
     **COMMON_CONFIG
 )
